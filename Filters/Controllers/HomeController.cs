72a61a52a8416b50da71b57e2c93dcfe895c799b
Максим Рожок <max15071999@gmail.com>
2018-08-18 15:15:33 +0300

    Add project files.

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Filters.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filters.Controllers
+{
+    //[RequireHttps]
+    //[HttpsOnly]
+    //[Profile]
+    //[ViewResultDetails]
+    //[RangeException]
+    //[TypeFilter(typeof(DiagnosticsFilter))]
+    //[TypeFilter(typeof(TimeFilter))]
+    //[ServiceFilter(typeof(TimeFilter))]
+    [Message("This is the Controller-Scoped Filter",Order=10)]
+    public class HomeController : Controller
+    {
+        //[RequireHttps]
+        [Message("This is the First Action-Scoped Filter",Order = 1)]
+        [Message("This is the Second Action-Scoped Filter",Order = -1)]
+        public ViewResult Index()
+        {
+            return View("Message", "This is the Index action on the Home controller");
+        }
+
+        //[RequireHttps]
+        public ViewResult SecondAction()
+        {
+            return View("Message", "This is the SecondAction action on the Home controller");
+        }
+
+        public ViewResult GenerateException(int? id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+            else if (id > 10)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                return View("Message", $"The value is {id}");
+            }
+        }
+    }
+}
---
+++ Filters/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Filters.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Filters
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddScoped<IFilterDiagnostics, DefaultFilterDiagnistics>();
+
+            //services.AddSingleton<IFilterDiagnostics, DefaultFilterDiagnistics>();
+            //services.AddSingleton<TimeFilter>();
+            //services.AddMvc();
+
+            services.AddScoped<IFilterDiagnostics, DefaultFilterDiagnistics>();
+            services.AddScoped<TimeFilter>();
+            services.AddScoped<ViewResultDiagnostics>();
+            services.AddScoped<DiagnosticsFilter>();
+            services.AddMvc().AddMvcOptions(options =>
+            {
+                //options.Filters.AddService(typeof(ViewResultDiagnostics));
+                //options.Filters.AddService(typeof(DiagnosticsFilter));
+                options.Filters.Add(new MessageAttribute("This is the Globally-Scoped Filter"));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
